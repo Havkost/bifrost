692f8197a78d62af6f9d0dea6eb8cc64389ed58e
JackSMunn <jmunn21@student.aau.dk>
2023-03-21 11:24:36 +0100

    DCL skub

--- src/ASTVisitor/ASTnodes/BoolskDcl.java
+++ src/ASTVisitor/ASTnodes/BoolskDcl.java
@@ -1,4 +1,12 @@
 package ASTVisitor.ASTnodes;
 
+import ASTVisitor.Parser.Visitor;
+
 public class BoolskDcl extends SymDeclaring {
+    public BoolskDcl(String i) {
+        id = i;
+    }
+
+    public void accept(Visitor v){v.visit(this);}
+
 }
--- src/ASTVisitor/ASTnodes/HeltalDcl.java
+++ src/ASTVisitor/ASTnodes/HeltalDcl.java
@@ -1,4 +1,13 @@
 package ASTVisitor.ASTnodes;
 
+import ASTVisitor.Parser.Visitor;
+
 public class HeltalDcl extends SymDeclaring {
+
+    public HeltalDcl(String i) {
+        id = i;
+    }
+
+    public void accept(Visitor v){v.visit(this);}
+
 }
