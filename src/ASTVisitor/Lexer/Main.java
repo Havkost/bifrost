edb9e8f8dd434f749b664f482cadc02195a3bee5
Havkost <August.kuhn@gmail.com>
2023-03-22 13:37:39 +0100

    Feat: Read sourcecode from file
    Remember to add path to source file to the Intellij Run configuration.

--- src/ASTVisitor/Lexer/Main.java
+++ src/ASTVisitor/Lexer/Main.java
@@ -6,25 +6,39 @@ import ASTVisitor.Parser.CCodeGenerator;
 import ASTVisitor.Parser.Prettyprinting;
 
 import java.io.CharArrayReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String example =
-                "gem heltal 3 som a\n" +
-                "hvis 10 er 10: \n" +
-                "   sæt a til 2. \n" +
-                "print a";
+        StringBuilder sourceString = new StringBuilder();
 
+        // Read text from source file into sourceString
+        // TODO: Evt. skriv bedre error handling og fejlbeskeder
+        try {
+            File sourceFile = new File(args[0]);
+            Scanner scanner = new Scanner(sourceFile);
+            while (scanner.hasNextLine()) {
+                sourceString.append(scanner.nextLine()).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("[FEJL] Kunne ikke finde filen '"+args[0]+"'.");
+            return;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("[FEJL] Kildefil blev ikke specificeret.");
+            return;
+        }
 
         try {
             System.out.println("\n=======================");
             System.out.println("Input program:");
             System.out.println("=======================");
-            System.out.println(example);
+            System.out.println(sourceString);
 
-            CharArrayReader reader = new CharArrayReader(example.toCharArray());
+            CharArrayReader reader = new CharArrayReader(sourceString.toString().toCharArray());
             CharStream charStream = new CharStream(reader);
 
             CodeScanner.initialize(charStream);
@@ -48,7 +62,7 @@ public class Main {
 
 
         } catch (Throwable e) {
-            System.out.println("Ended with error: " + e);
+            System.out.println("[FEJL]: " + e);
             System.out.println("Stack trace:\n");
             e.printStackTrace(System.out);
         }
