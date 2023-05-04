cd2af1aaf664f71d33408a85da5e9b985fb9643c
Jatewo <jacobterpewoods@gmail.com>
2023-05-04 10:20:35 +0200

    Update WindowsScript.java

--- src/ASTVisitor/Scripting/WindowsScript.java
+++ src/ASTVisitor/Scripting/WindowsScript.java
@@ -5,19 +5,20 @@ import java.io.IOException;
 
 public class WindowsScript implements Script {
     @Override
-    public void compileCFile(String fileName) {
-
+    public void compileCFile(String fileName) throws IOException {
+        sendCommand(createTempScript("gcc -L./Lib -l eziotlib " + fileName));
     }
 
     @Override
-    public void runProgram() {
+    public void runProgram() throws IOException {
 
+        sendCommand(createTempScript("a.exe"));
     }
 
     @Override
     public void sendCommand(File tempScript) {
         try {
-            ProcessBuilder pb = new ProcessBuilder("bash", tempScript.toString());
+            ProcessBuilder pb = new ProcessBuilder("cmd", tempScript.toString());
             pb.inheritIO();
             Process process = pb.start();
             process.waitFor();
