e9e5a0077df7adfdf8db7026723ec38b7619e22c
Havkost <August.kuhn@gmail.com>
2023-05-10 10:47:02 +0200

    Add curl functions to c library

--- Lib/Eziot.h
+++ Lib/Eziot.h
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <curl/curl.h>
+#include <cjson/cJSON.h>
 
 typedef struct {
     int hour;
@@ -14,3 +16,13 @@ Time make_time(int hour, int minute);
 char* concat(char* str1, char* str2);
 int customStrLen(char* str1, int len2);
 Time time_generator();
+
+enum Datatype {
+    TYPE_INTEGER,
+    TYPE_DOUBLE,
+    TYPE_STRING,
+    TYPE_BOOL
+};
+
+int send_field_to_endpoint(char *endpoint, char *field, void *value_ptr, enum Datatype datatype);
+int get_field_from_endpoint(char *endpoint, char *field, void *value_ptr, enum Datatype datatype);
