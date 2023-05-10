86d80d2573bc54947ae72418596363f0cfe1f146
Havkost <August.kuhn@gmail.com>
2023-05-10 13:23:53 +0200

    Add event loop code to Custom_library

--- Lib/Custom_library.c
+++ Lib/Custom_library.c
@@ -194,3 +194,72 @@ int get_field_from_endpoint(char *endpoint, char *field, void *value_ptr, enum D
     
     return 0;
 }
+
+// Event loop
+
+/*
+    If statement structs
+*/
+
+void init_if_statement(if_statement *statement, void *condition, void *body) {
+    statement->condition = condition;
+    statement->body = body;
+    statement->last_state = false;
+}
+
+/*
+    If statement queues
+*/
+
+void init_if_queue(if_queue *queue) {
+    queue->head = -1;
+    queue->tail = -1;
+}
+
+bool is_queue_full(if_queue *queue) {
+    return (queue->tail+1) % IF_QUEUE_SIZE == queue->head;
+}
+
+bool is_queue_empty(if_queue *queue) {
+    return queue->head == -1;
+}
+
+bool add_to_queue(if_queue *queue, void (*element)()) {
+    if(is_queue_full(queue)) return false;
+    if(is_queue_empty(queue)) queue->head = 0;
+    queue->tail = (queue->tail+1)%IF_QUEUE_SIZE;
+    queue->if_bodies[queue->tail] = element;
+    return true;
+}
+
+bool remove_from_queue(if_queue *queue) {
+    if(is_queue_empty(queue)) return false;
+    if(queue->head == queue->tail) init_if_queue(queue);
+    else queue->head = (queue->head+1)%IF_QUEUE_SIZE;
+    return true;
+}
+
+void (*get_from_queue(if_queue *queue))() {
+    return queue->if_bodies[queue->head];
+}
+
+/*
+    Multi threading
+*/
+
+run_if_thread_args *init_run_if_thread_args(int *thread_count, void (*body)(), pthread_mutex_t *thread_count_lock) {
+    run_if_thread_args *args = malloc(sizeof(run_if_thread_args));
+    args->body = body;
+    args->thread_count = thread_count;
+    args->thread_count_lock = thread_count_lock;
+    return args;
+}
+
+void *run_if_thread(void *_args) {
+    run_if_thread_args *args = (run_if_thread_args *) _args;
+    args->body();
+    pthread_mutex_lock(args->thread_count_lock);
+    (*(args->thread_count))--;
+    pthread_mutex_unlock(args->thread_count_lock);
+    free(args);
+}
--- Lib/Eziot.h
+++ Lib/Eziot.h
@@ -6,6 +6,9 @@
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
 
+#include <pthread.h>
+#include <windows.h>
+
 typedef struct {
     int hour;
     int minute;
@@ -26,3 +29,41 @@ enum Datatype {
 
 int send_field_to_endpoint(char *endpoint, char *field, void *value_ptr, enum Datatype datatype);
 int get_field_from_endpoint(char *endpoint, char *field, void *value_ptr, enum Datatype datatype);
+
+// Event loop
+
+#define IF_QUEUE_SIZE 16
+#define MAX_THREADS 8
+
+// If statement structs
+typedef struct {
+    bool (*condition)();
+    void (*body)();
+    bool last_state;
+} if_statement;
+
+void init_if_statement(if_statement *statement, void *condition, void *body);
+
+// If statement queues
+typedef struct {
+    void (*if_bodies[IF_QUEUE_SIZE])();
+    int head;
+    int tail;
+} if_queue;
+
+void init_if_queue(if_queue *queue);
+bool is_queue_full(if_queue *queue);
+bool is_queue_empty(if_queue *queue);
+bool add_to_queue(if_queue *queue, void (*element)());
+bool remove_from_queue(if_queue *queue);
+void (*get_from_queue(if_queue *queue))();
+
+// Multi threading
+typedef struct {
+    int *thread_count;
+    void (*body)();
+    pthread_mutex_t *thread_count_lock;
+} run_if_thread_args;
+
+run_if_thread_args *init_run_if_thread_args(int *thread_count, void (*body)(), pthread_mutex_t *thread_count_lock);
+void *run_if_thread(void *_args);
